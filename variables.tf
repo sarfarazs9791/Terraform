597c7eef66d8d1b88afacf1527d23a8ee66e1179
sarfarazs9791 <45591490+sarfarazs9791@users.noreply.github.com>
2022-01-07 10:02:35 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,16 +1,1 @@
-variable "region" {
-  type = list(string)
-}
 
-variable "LBname" {
-  type = string
-  default = "LB1"
-}
-
-
-
-variable "sg_ports" {
-
-  type = list(number)
-  default = [8200,8201,8300,9200,9500]
-}
